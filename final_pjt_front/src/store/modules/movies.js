8350b785d5c6f9e7b2200def64014a982c039511
GYEONGYEOL PARK <hrq789@naver.com>
2022-05-22 16:30:50 +0900

    vue-recommend

--- final_pjt_front/src/store/modules/movies.js
+++ final_pjt_front/src/store/modules/movies.js
@@ -10,19 +10,25 @@ export default {
     movies : [],
     movie: {},
 
+    recommendMovies: [],
+
   },
   getters: {
     isLoggedIn: state => !!state.token,
     currentUser: state => state.currentUser,
     movies: state => state.movies,
     movie: state => state.movie,
-    authHeader: state => ({ Authorization: `Token ${state.token}`})
+    authHeader: state => ({ Authorization: `Token ${state.token}`}),
+
+    recommendMovies: state => state.recommendMovies,
   },
   mutations: {
     // SET_TOKEN: (state, token) => state.token = token,
     SET_CURRENT_USER: (state, user) => state.currentUser = user,
     SET_MOVIES: (state, movies) => state.movies = movies,
-    SET_MOVIE: (state, movie) => state.movie = movie
+    SET_MOVIE: (state, movie) => state.movie = movie,
+
+    SET_RECOMMEND_MOVIES: (state, recommendMovies) => state.recommendMovies = recommendMovies,
   },
   actions: {
     movieDetail({ getters, commit }, { moviePk }) {
@@ -36,6 +42,21 @@ export default {
           commit('SET_MOVIE', res.data)
         })
         .catch(err => console.log(err.response.data))
-    }
+    },
+
+    getRecommendMovies({ commit, getters }) {
+
+      axios({
+        url : drf.movies.movie_list(),
+        method: 'get',
+        headers: getters.authHeader,
+      })
+        .then(res => commit('SET_RECOMMEND_MOVIES', res.data))
+        .catch(err => {
+          console.error(err.response)
+        })
+    },
+
+    // getSeachMovies({}) {}
   }
 }
