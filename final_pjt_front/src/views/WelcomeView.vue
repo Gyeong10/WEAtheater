4825183a722c8c39bb97319565350e8b217cac18
GYEONGYEOL PARK <hrq789@naver.com>
2022-05-24 18:12:28 +0900

    signup

--- final_pjt_front/src/views/WelcomeView.vue
+++ final_pjt_front/src/views/WelcomeView.vue
@@ -1,18 +1,27 @@
 <template>
   <div>
-    <h2> 영화 취향을 알려주세요!</h2>
+    <h2> 영화 취향을 알려주세요! (3개이상!) </h2>
     <ul>
       <li v-for="(movie, idx) in allMovieList.slice(0, 19)" :key="idx">
         <div>
           <img :src="`https://www.themoviedb.org/t/p/w440_and_h660_face/${movie.poster_url}`" alt="poster">
           {{ movie.title }}
-          <button @click="likeMovie({moviePk : movie.pk})">
-            선택
-          </button>
+          <div v-if="userData in movie.like_users">
+            <button @click="likeMovie({moviePk : movie.pk})">
+              선택 해제
+            </button>
+          </div>
+          <div v-else>
+            <button @click="likeMovie({moviePk : movie.pk})">
+              선택{{ movie.like_users }}, {{ currentUser.pk }}, {{ currentUser.username }}
+            </button>
+          </div>
         </div>
       </li>
     </ul>
+    
     <router-link :to="{ name: 'home' }">Home</router-link>
+
   </div>
 </template>
 
@@ -21,8 +30,16 @@ import { mapActions, mapGetters } from 'vuex'
 
 export default {
   name: 'WelcomeView',
+  data() {
+    return {
+      userData: {
+        pk: this.currentUser.pk,
+        username: this.currentUser.username
+      }
+    }
+  },
   computed: {
-    ...mapGetters(['allMovieList']),
+    ...mapGetters(['allMovieList', 'currentUser']),
   },
   methods: {
     ...mapActions(['getAllMovies', 'likeMovie']),
