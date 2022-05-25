9677ea94226afee1924221d96d1e6111eb374547
GYEONGYEOL PARK <hrq789@naver.com>
2022-05-25 09:12:38 +0900

    movie_data.json

--- final_pjt_back/init.py
+++ final_pjt_back/init.py
@@ -9,46 +9,47 @@ def get_movie_datas():
     total_data = []
     num = 1
     movie_id = 0
-    for i in range(1, 10):
+    for i in range(1, 501):
         movies_url = f"https://api.themoviedb.org/3/movie/popular?api_key={TMDB_API_KEY}&language=ko-KR&page={i}"
         movies = requests.get(movies_url).json()
-        for movie in movies['results']:
-            movie_id = movie['id']
-            details_url = f'https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key={TMDB_API_KEY}&language=ko-KR'
-            movie_details = requests.get(details_url).json()
-            actor_list = []
-            if 'cast' in movie_details.keys():
-                for person in movie_details['cast']:
-                    cnt = 0
-                    if cnt <= 5:
-                        if 'known_for_department' in person.keys() and 'popularity' in person.keys():
-                            if person['known_for_department'] == 'Acting' and person['popularity'] >= 10:
-                                actor_list.append(person['id'])
-                                all_actors.add((person['id'], person['name']))
-                                cnt += 1
-                    else:
-                        break
-            if movie.get('release_date', ''):
-                fields = {
-                    'movie_id': movie['id'],
-                    'title': movie['title'],
-                    'release_date': movie['release_date'],
-                    'popularity': movie['popularity'],
-                    'vote_average': movie['vote_average'],
-                    'overview': movie['overview'],
-                    'poster_url': movie['poster_path'],
-                    'genres': movie['genre_ids'],
-                    'actors' : actor_list,
-                }
+        if 'results' in movies.keys():
+            for movie in movies['results']:
+                movie_id = movie['id']
+                details_url = f'https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key={TMDB_API_KEY}&language=ko-KR'
+                movie_details = requests.get(details_url).json()
+                actor_list = []
+                if 'cast' in movie_details.keys():
+                    for person in movie_details['cast']:
+                        cnt = 0
+                        if cnt <= 5:
+                            if 'known_for_department' in person.keys() and 'popularity' in person.keys() and 'id' in person.keys() and 'name' in person.keys():
+                                if person['known_for_department'] == 'Acting' and person['popularity'] >= 10:
+                                    actor_list.append(person['id'])
+                                    all_actors.add((person['id'], person['name']))
+                                    cnt += 1
+                        else:
+                            break
+                if movie.get('release_date', ''):
+                    fields = {
+                        'movie_id': movie['id'],
+                        'title': movie['title'],
+                        'release_date': movie['release_date'],
+                        'popularity': movie['popularity'],
+                        'vote_average': movie['vote_average'],
+                        'overview': movie['overview'],
+                        'poster_url': movie['poster_path'],
+                        'genres': movie['genre_ids'],
+                        'actors' : actor_list,
+                    }
 
-                data = {
-                    "pk": num,
-                    "model": "movies.movie",
-                    "fields": fields
-                }
+                    data = {
+                        "pk": num,
+                        "model": "movies.movie",
+                        "fields": fields
+                    }
 
-                total_data.append(data)
-            num += 1
+                    total_data.append(data)
+                num += 1
 
     with open("movies/fixtures/movie_data.json", "w", encoding="utf-8") as w:
         json.dump(total_data, w, indent=4, ensure_ascii=False)
