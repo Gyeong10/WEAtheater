1d520e77f5784536adc81fc88b273fab830d2ddc
GYEONGYEOL PARK <hrq789@naver.com>
2022-05-26 00:30:34 +0900

    welcome page

--- final_pjt_front/src/views/WelcomeView.vue
+++ final_pjt_front/src/views/WelcomeView.vue
@@ -1,33 +1,181 @@
 <template>
   <div>
-    <h2> 영화 취향을 알려주세요! (3개이상!) </h2>
-    <ul>
-      <li v-for="(movie, idx) in allMovieList.slice(0, 19)" :key="idx">
-        <div>
-          <img :src="`https://www.themoviedb.org/t/p/w440_and_h660_face/${movie.poster_url}`" alt="poster">
-          {{ movie.title }}
+    <h2> 영화 취향을 알려주세요! </h2>
+        <!-- <div v-for="(movie, idx) in allMovieList.slice(0, 19)" :key="idx">
+    <div id="card">
+    <v-card
+      class="mx-auto rounded-xl"
+      max-width="300"
+    >
+        <v-img
+          class="rounded-t-xl"
+          height="500px"
+          :src="`https://image.tmdb.org/t/p/w500${movie.poster_url}`"
+        >
+        </v-img>
+        <v-card-subtitle class="text-decoration-none">
+          {{ movie.title }} 
           <v-btn
               color="primary"
               @click="hidden = !hidden, likeMovie({moviePk: movie.pk})"
             >
               {{ hidden ? '취소' : '선택' }}
             </v-btn>
+        </v-card-subtitle>
+    </v-card>
+    </div>
+        </div> -->
+    <v-stepper v-model="e1">
+    <v-stepper-header>
+      <v-stepper-step
+        :complete="e1 > 1"
+        step="1"
+      >
+        영화 선택 1
+      </v-stepper-step>
+
+      <v-divider></v-divider>
+
+      <v-stepper-step
+        :complete="e1 > 2"
+        step="2"
+      >
+        영화 선택 2
+      </v-stepper-step>
+
+      <v-divider></v-divider>
+
+      <v-stepper-step step="3">
+        영화 선택 3
+      </v-stepper-step>
+    </v-stepper-header>
+
+    <v-stepper-items>
+      <v-stepper-content step="1">
+        <v-card
+          class="mb-12"
+          height="1300px"
+          width="1500px"
+        >
+        <v-container class="d-flex">
+        <div v-for="(movie, idx) in allMovieList.slice(0, 6)" :key="idx">
+    <div id="card">
+    <v-card
+      class="mx-auto rounded-xl"
+      max-width="300"
+    >
+        <v-img
+          class="rounded-t-xl"
+          height="500px"
+          :src="`https://image.tmdb.org/t/p/w500${movie.poster_url}`"
+        >
+        </v-img>
+        <v-card-subtitle class="text-decoration-none">
+          {{ movie.title }} 
+          <v-btn
+          color="primary"
+          @click="e1 = 2, likeMovie({moviePk: movie.pk})"
+        >
+          선택
+        </v-btn>
+        </v-card-subtitle>
+    </v-card>
+    </div>
         </div>
-      </li>
-    </ul>
-    <router-link :to="{ name: 'home' }">Home</router-link>
+        
+        </v-container>
+        </v-card>
+      </v-stepper-content>
+
+      <v-stepper-content step="2">
+        <v-card
+          class="mb-12"
+          height="1300px"
+          width="1500px"
+        >
+        <v-container class="d-flex">
+        <div v-for="(movie, idx) in allMovieList.slice(7, 13)" :key="idx">
+    <div id="card">
+    <v-card
+      class="mx-auto rounded-xl"
+      max-width="300"
+    >
+        <v-img
+          class="rounded-t-xl"
+          height="500px"
+          :src="`https://image.tmdb.org/t/p/w500${movie.poster_url}`"
+        >
+        </v-img>
+        <v-card-subtitle class="text-decoration-none">
+          {{ movie.title }} 
+          <v-btn
+          color="primary"
+          @click="e1 = 3, likeMovie({moviePk: movie.pk})"
+        >
+          선택
+        </v-btn>
+        </v-card-subtitle>
+    </v-card>
+    </div>
+        </div>
+        
+        </v-container>
+        </v-card>
+
+      </v-stepper-content>
+
+      <v-stepper-content step="3">
+        <v-card
+          class="mb-12"
+          height="1300px"
+          width="1500px"
+        >
+        <v-container class="d-flex">
+        <div v-for="(movie, idx) in allMovieList.slice(14, 19)" :key="idx">
+    <div id="card">
+    <v-card
+      class="mx-auto rounded-xl"
+      max-width="300"
+    >
+        <v-img
+          class="rounded-t-xl"
+          height="500px"
+          :src="`https://image.tmdb.org/t/p/w500${movie.poster_url}`"
+        >
+        </v-img>
+        <v-card-subtitle class="text-decoration-none">
+          {{ movie.title }} 
+          <v-btn
+          color="primary"
+          @click="likeMovie({moviePk: movie.pk})"
+        >
+          <router-link :to="{ name: 'home' }" class="text-decoration-none white--text">선택</router-link>
+        </v-btn>
+        </v-card-subtitle>
+    </v-card>
+    </div>
+        </div>
+        
+        </v-container>
+        </v-card>
+
+      </v-stepper-content>
+    </v-stepper-items>
+  </v-stepper>
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
 
+
 export default {
   name: 'WelcomeView',
   data() {
     return {
-      hidden: false,
-      // hidden: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
+
+      e1: 1,
+    
     }
   },
   computed: {
@@ -42,6 +190,13 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.d-flex {
+  flex-flow: row wrap;
+  justify-content: center;
+}
 
+#id {
+  margin: 1rem;
+}
 </style>
